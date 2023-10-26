9e490026e6a62efb05d33748d6e8bc1061d06493
Rojukugen <wizililqq@gmail.com>
2023-10-26 08:30:23 +0300

    Update test_Yashin_Egor_complex_number.cpp

--- modules/complex-number/test/test_Yashin_Egor_complex_number.cpp
+++ modules/complex-number/test/test_Yashin_Egor_complex_number.cpp
@@ -5,34 +5,22 @@
 #include "include/complex_number.h"
 
 
-// Test power method to the first power (identity)
-TEST(Yashin_EGOR_ComplexNumberTest, PowerIdentity) {
-	ComplexNumber z(2.0, -3.0);
-	ComplexNumber identity = z.power(1);
-	EXPECT_EQ(identity.getRe(), 2.0);
-	EXPECT_EQ(identity.getIm(), -3.0);
+TEST(Yashin_Egor_Co1mplexNumberTest, Default_Constructor) {
+    ComplexNumber number;
+    ASSERT_EQ(number.getIm(), 0);
+    ASSERT_EQ(number.getRe(), 0);
 }
-
-// Test power method to the zero power
-TEST(Yashin_EGOR_ComplexNumberTest, PowerZero) {
-	ComplexNumber z(2.0, -3.0);
-	ComplexNumber zeroPower = z.power(0);
-	EXPECT_EQ(zeroPower.getRe(), 1.0);
-	EXPECT_EQ(zeroPower.getIm(), 0.0);
+TEST(Yashin_Egor_ComplexNumberTest, Setters) {
+    ComplexNumber number;
+    number.setRe(8);
+    number.setIm(9);
+    ASSERT_EQ(number.getRe(), 8);
+    ASSERT_EQ(number.getIm(), 9);
 }
-
-// Test power method to a negative power
-TEST(Yashin_EGOR_ComplexNumberTest, PowerNegative) {
-	ComplexNumber z(2.0, -3.0);
-	ComplexNumber negativePower = z.power(-2);
-	EXPECT_DOUBLE_EQ(negativePower.getRe(), -0.045454545454545456);
-	EXPECT_DOUBLE_EQ(negativePower.getIm(), 0.06818181818181819);
-}
-
-// Test power method to a fractional power
-TEST(Yashin_EGOR_ComplexNumberTest, PowerFractional) {
-	ComplexNumber z(2.0, -3.0);
-	ComplexNumber fractionalPower = z.power(0.5);
-	EXPECT_DOUBLE_EQ(fractionalPower.getRe(), 1.5010595677132415);
-	EXPECT_DOUBLE_EQ(fractionalPower.getIm(), -0.9039072313340563);
+TEST(Yashin_Egor_ComplexNumberTest, Modified_Sum_Conjugate) {
+    ComplexNumber num1(42, 24);
+    ComplexNumber num2(42, -24);
+    ComplexNumber num3 = num1 + num2;
+    ASSERT_EQ(num3.getRe(), 84);
+    ASSERT_EQ(num3.getIm(), 0);
 }
