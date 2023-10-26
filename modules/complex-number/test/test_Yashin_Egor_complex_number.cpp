e863d9e0ec3ffc41aa9e8fe51e530188d79274a4
Rojukugen <wizililqq@gmail.com>
2023-10-26 08:34:00 +0300

    Update test_Yashin_Egor_complex_number.cpp

--- modules/complex-number/test/test_Yashin_Egor_complex_number.cpp
+++ modules/complex-number/test/test_Yashin_Egor_complex_number.cpp
@@ -5,22 +5,26 @@
 #include "include/complex_number.h"
 
 
-TEST(Yashin_Egor_Co1mplexNumberTest, Default_Constructor) {
-    ComplexNumber number;
-    ASSERT_EQ(number.getIm(), 0);
-    ASSERT_EQ(number.getRe(), 0);
+TEST(Yashin_Egor_ComplexNumberTest, Can_Get_Im) {
+    double re = 2.0;
+    double im = 3.0;
+    ComplexNumber z(re, im);
+    EXPECT_DOUBLE_EQ(im, z.getIm());
 }
-TEST(Yashin_Egor_ComplexNumberTest, Setters) {
-    ComplexNumber number;
-    number.setRe(8);
-    number.setIm(9);
-    ASSERT_EQ(number.getRe(), 8);
-    ASSERT_EQ(number.getIm(), 9);
+TEST(Yashin_Egor_ComplexNumberTest, TEST_SUB) {
+    ComplexNumber a(123.0, 50.0),
+        b(30.0, 14.0),
+        expected(93.0, 36.0);
+
+    ComplexNumber result = a - b;
+
+    ASSERT_EQ(expected, result);
 }
-TEST(Yashin_Egor_ComplexNumberTest, Modified_Sum_Conjugate) {
+TEST(Yashin_Egor_ComplexNumberTest, Sum_Conjugate) {
     ComplexNumber num1(42, 24);
     ComplexNumber num2(42, -24);
-    ComplexNumber num3 = num1 + num2;
-    ASSERT_EQ(num3.getRe(), 84);
-    ASSERT_EQ(num3.getIm(), 0);
+
+    ComplexNumber num3(84, 0);
+
+    ASSERT_EQ(num1 + num2, num3);
 }
