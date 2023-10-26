f7ef0ed82507a845789863acb2be1553fb715c9e
Rojukugen <wizililqq@gmail.com>
2023-10-26 07:49:51 +0300

    Update test_Yashin_Egor_complex_number.cpp

--- modules/complex-number/test/test_Yashin_Egor_complex_number.cpp
+++ modules/complex-number/test/test_Yashin_Egor_complex_number.cpp
@@ -5,43 +5,62 @@
 #include "include/complex_number.h"
 
 
-TEST(Yashin_Egor_ComplexNumberTest, can_add_large_complex_numbers) {
-    ComplexNumber num1(1.0e6, 2.0e6);
-    ComplexNumber num2(-0.5e6, -0.5e6);
-    ComplexNumber sum = num1 + num2;
-    ASSERT_NEAR(sum.getRe(), 0.5e6, 1e-6);
-    ASSERT_NEAR(sum.getIm(), 1.5e6, 1e-6);
+TEST(Silaeva_Natalya_ComplexNumberTest1, Can_Create_Complex_Number) {
+    double re = 2.0;
+    double im = 0.1;
+
+    ComplexNumber res(re, im);
+
+    EXPECT_EQ(re, res.getRe());
+    EXPECT_EQ(im, res.getIm());
 }
 
-TEST(Yashin_Egor_ComplexNumberTest, can_multiply_complex_numbers_with_imaginary_units) {
-    ComplexNumber i(0.0, 1.0);
-    ComplexNumber result = i * i;
-    ASSERT_NEAR(result.getRe(), -1.0, 1e-6);
-    ASSERT_NEAR(result.getIm(), 0.0, 1e-6);
+TEST(Silaeva_Natalya_ComplexNumberTest1, Can_Multiply_Complex_Numbers) {
+    double re1 = 0.3;
+    double im1 = 0.1;
+
+    double re2 = 0.8;
+    double im2 = 0.6;
+
+    ComplexNumber cnum1(0.3, 0.1);
+    ComplexNumber cnum2(0.8, 0.6);
+    ComplexNumber res = cnum1 * cnum2;
+
+    EXPECT_EQ(0.18, res.getRe());
+    EXPECT_EQ(0.26, res.getIm());
 }
 
+TEST(Silaeva_Natalya_ComplexNumberTest1, Can_Sum_Complex_Numbers) {
+    double re1 = 2.0;
+    double im1 = 0.4;
 
+    ComplexNumber cnum1(2.0, 0.4);
 
-TEST(Yashin_Egor_ComplexNumberTest, can_copy_construct_complex_number) {
-    ComplexNumber num(3.0, 4.0);
-    ComplexNumber copy = num;
-    ASSERT_EQ(copy.getRe(), 3.0);
-    ASSERT_EQ(copy.getIm(), 4.0);
+    double re2 = 6.2;
+    double im2 = 0.4;
+
+    ComplexNumber cnum2(6.2, 0.4);
+
+    ComplexNumber res = cnum1 + cnum2;
+
+    EXPECT_EQ(8.2, res.getRe());
+    EXPECT_EQ(0.8, res.getIm());
 }
 
-TEST(Yashin_Egor_ComplexNumberTest, can_assign_complex_number) {
-    ComplexNumber num1(1.0, 2.0);
-    ComplexNumber num2(3.0, 4.0);
-    num1 = num2;
-    ASSERT_EQ(num1.getRe(), 3.0);
-    ASSERT_EQ(num1.getIm(), 4.0);
-}
+TEST(Silaeva_Natalya_ComplexNumberTest1, Can_Subtract_Complex_Numbers) {
+    double re1 = 2.0;
+    double im1 = 0.4;
 
-TEST(Yashin_Egor_ComplexNumberTest, can_compare_complex_numbers) {
-    ComplexNumber num1(2.0, 3.0);
-    ComplexNumber num2(2.0, 3.0);
-    ComplexNumber num3(4.0, 5.0);
-    ASSERT_TRUE(num1 == num2);
-    ASSERT_FALSE(num1 == num3);
+    ComplexNumber cnum1(2.0, 0.4);
+
+    double re2 = 6.2;
+    double im2 = 0.4;
+
+    ComplexNumber cnum2(6.2, 0.4);
+
+    ComplexNumber res = cnum2 - cnum1;
+
+    EXPECT_EQ(4.2, res.getRe());
+    EXPECT_EQ(0.0, res.getIm());
 }
 
