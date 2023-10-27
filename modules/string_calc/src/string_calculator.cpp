d1f4efa6ec5a6d58e5086911a3b0f26f5a128a89
Rojukugen <wizililqq@gmail.com>
2023-10-28 02:42:22 +0300

    case

--- modules/string_calc/src/string_calculator.cpp
+++ modules/string_calc/src/string_calculator.cpp
@@ -111,30 +111,17 @@ bool CalcStringCalculator::CalcCheck() {
 
 int CalcStringCalculator::CalcPriority(const char& el) {
     switch (el) {
-    case '(':
-    {
-        return 0;
-    }
-    case '+':
-    {
-        return 1;
-    }
-    case '-':
-    {
-        return 1;
-    }
-    case '*':
-    {
-        return 2;
-    }
-    case '/':
-    {
-        return 2;
-    }
-    case '^':
-    {
-        return 3;
-    }
+    case '(':return 0;
+    
+    case '+':return 1;
+    
+    case '-':return 1;
+
+    case '*':return 2;
+   
+    case '/':return 2;
+    
+    case '^':return 3;
     }
     return -1;
 }
